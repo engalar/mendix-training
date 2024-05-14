522bbeb2e3c38b191596d4b59ef9820f4b23f8aa
engalar <engalar@gmail.com>
2024-05-14 11:16:42 +0800

    implement widget

--- pluggable-widget/projects/groupBox/src/GroupBox.tsx
+++ pluggable-widget/projects/groupBox/src/GroupBox.tsx
@@ -5,6 +5,6 @@ import { GroupBoxContainerProps } from "../typings/GroupBoxProps";
 
 import "./ui/GroupBox.css";
 
-export function GroupBox({ sampleText }: GroupBoxContainerProps): ReactElement {
-    return <HelloWorldSample sampleText={sampleText ? sampleText : "World"} />;
+export function GroupBox({ sampleText, slogan }: GroupBoxContainerProps): ReactElement {
+    return <HelloWorldSample sampleText={sampleText ? sampleText : "World"} slogan={slogan} />;
 }
--- pluggable-widget/projects/groupBox/src/components/HelloWorldSample.tsx
+++ pluggable-widget/projects/groupBox/src/components/HelloWorldSample.tsx
@@ -2,8 +2,9 @@ import { ReactElement, createElement } from "react";
 
 export interface HelloWorldSampleProps {
     sampleText?: string;
+    slogan: string;
 }
 
-export function HelloWorldSample({ sampleText }: HelloWorldSampleProps): ReactElement {
-    return <div className="widget-hello-world">Hello {sampleText}</div>;
+export function HelloWorldSample({ sampleText, slogan }: HelloWorldSampleProps): ReactElement {
+    return <div className="widget-hello-world">Hello {sampleText}, Slogan: {slogan}</div>;
 }
