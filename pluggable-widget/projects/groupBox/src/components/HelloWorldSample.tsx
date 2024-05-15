21166f9bf901f6886b472323526b328672f2215d
engalar <engalar@gmail.com>
2024-05-15 11:23:21 +0800

    静态多语言

--- pluggable-widget/projects/groupBox/src/components/HelloWorldSample.tsx
+++ pluggable-widget/projects/groupBox/src/components/HelloWorldSample.tsx
@@ -1,9 +1,14 @@
-import { ReactElement, createElement } from "react";
+import { ReactElement, createElement, useMemo } from "react";
 
 export interface HelloWorldSampleProps {
     sampleText?: string;
 }
 
 export function HelloWorldSample({ sampleText }: HelloWorldSampleProps): ReactElement {
-    return <div className="widget-hello-world">Hello {sampleText}</div>;
+    const welcome = useMemo(() => {
+        // @ts-ignore
+        const locale = mx.session.getConfig('locale.code') || 'en_CN';
+        return locale === 'zh_CN' ? '欢迎' : locale === 'zh_TW' ? '歡迎' : 'Welcome';
+    }, []);
+    return <div className="widget-hello-world">{welcome} {sampleText}</div>;
 }
